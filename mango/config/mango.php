7643c81f1d33ff282486da535ce312a58c066f7f
Wouter <wouter.w@gmx.net>
2009-07-21 04:15:24 +0200

    forgot to add the config file (removed the mango_ext config file when I merged mango & mango_ext, but forget to add the new mango config file)

---
+++ mango/config/mango.php
@@ -0,0 +1,18 @@
+<?php
+
+// Inheritance data
+$config['cti'] = array(
+	'car' =>  array(
+		'car_type' => array(
+			1 => 'Spyker',
+			2 => 'Ferrari'
+		)
+	),
+	// you could specify even further extension
+	// using the same pattern
+	// 'spyker' => array( etc
+);
+
+
+
+?>
